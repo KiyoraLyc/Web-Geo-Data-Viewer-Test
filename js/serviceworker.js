f57e4b3e755ccb76cce43b5e36dc3eecb278010c
Ching <kiyoralyc@gmail.com>
2022-02-13 23:51:23 +0800

    Add Pwa Page

---
+++ js/serviceworker.js
@@ -0,0 +1,41 @@
+﻿var staticCacheName = "ChinHsinCleanupAppPWA";
+self.addEventListener("install", function (event) {
+    console.log("install");
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log("activate");
+});
+self.addEventListener('updatefound', function (event) {
+    console.log("updatefound");
+});
+self.addEventListener('statechange', function (event) {
+    console.log("statechange");
+});
+self.addEventListener('controllerchange', function (event) {
+    console.log("controllerchange");
+});
+self.addEventListener('message', function (event) {
+    console.log("message");
+});
+self.addEventListener('fetch', function (event) {
+    console.log("fetch")
+    event.respondWith(async function () {
+        try {
+            var res = await fetch(event.request);
+            var cache = await caches.open('cache');
+            cache.put(event.request.url, res.clone());
+            //console.log('pwa fetch', res);
+            return res;
+        }
+        catch (error) {
+            console.error('pwa fetch error', error);
+            return caches.match(event.request);
+        }
+    }());
+});
---
+++ js/useServiceworker.js
@@ -0,0 +1,20 @@
+﻿window.addEventListener('load', () => {
+    registerSW();
+});
+// Register the Service Worker
+function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            if ('launchQueue' in window && 'files' in LaunchParams.prototype) {
+                console.log("files");
+              }
+            navigator.serviceWorker.register('serviceworker.js').then(function (reg) {
+
+            });
+        }
+        catch (error) {
+            console.log('SW registration failed');
+            alert('SW registration failed');
+        }
+    }
+}
